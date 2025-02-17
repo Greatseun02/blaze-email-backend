7a38cf26aa0dac90a34eeea8cb805f8a77de0ae5
Goodnews Adewole <goodnews.adewole@example.com>
2025-02-17 19:08:35 +0100

    feat: set transaction email with mailtrap and nodemailer

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+require('dotenv').config();
+
+// const transporter = nodemailer.createTransport({
+//     service: "Gmail", 
+//     auth: {
+//     user: "mygmail@gmail.com",
+//     pass: "password"
+//     }
+// });
+
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    secure:false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    },
+});
+
+
+app.post("/api/send", (req, res) => {
+    const mailOptions = {
+        from: "hello@demomailtrap.com",
+        to: req.body.to,
+        subject: req.body.subject,
+        html: req.body.message
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            return res.status(500).send(error);
+        }
+        res.status(200).send("Email sent successfully");
+    });
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
